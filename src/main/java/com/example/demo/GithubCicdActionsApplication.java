e0fe30e17c8aa284219ea891759edd64f16cff86
Pradeep Deshmukhmane <pdeshmukhmane@synamedia.com>
2023-08-15 18:16:42 +0530

    inital code changes

---
+++ src/main/java/com/example/demo/GithubCicdActionsApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+	
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welome to CICD using gitacions";
+	}
+
+}
